eb352a804e25ee38a4ce3c9a151e6372236c0753
RyanDavari <ryan.davari@gmail.com>
2026-01-05 14:08:28 +1100

    Mapping profile added

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -6,6 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
+// Automapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 // In Memory Database
 builder.Services.AddDbContext<AppDbContext>(opt => 
     opt.UseInMemoryDatabase("InMem"));
