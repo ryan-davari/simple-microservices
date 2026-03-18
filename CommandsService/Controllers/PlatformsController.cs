02d0e77afeedb0f5e3bcf9b8d4b959e9ae11f1d4
Ryan Davari <ryan.davari@gmail.com>
2026-03-18 16:50:23 +1100

    Command Controller and Repositories added

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,10 +11,25 @@ namespace CommandsService.Controllers
     [ApiController]
     public class PlatformsController : ControllerBase
     {
-        public PlatformsController()
+        private readonly ICommandRepo _repository;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(ICommandRepo repository, IMapper mapper)
         {
-            
+            _repository = repository;
+            _mapper = mapper;
         }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting Platforms from CommandsService");
+
+            var platformItems = _repository.GetAllPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformreadDto>>(platformItems));
+        }
+
 
         [HttpPost]
         public ActionResult TestInboundConnection()
@@ -19,5 +37,9 @@ namespace CommandsService.Controllers
             Console.WriteLine("--> Inbound POST # Command Service");
             return Ok("Inbound test of from Platform Controller");
         }
+    }
+
+    internal class PlatformreadDto
+    {
     }
 }
